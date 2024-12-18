44e85ad869d928cc1e75b304968a9df0213cbc80
Tuan Nguyen <nguyendt03@gmail.com>
2024-12-18 16:50:08 +0800

    Rounding

---
+++ functionality/OddRound.cpp
@@ -0,0 +1,12 @@
+#include "OddRound.hpp"
+#include <math.h>
+
+double roundToHigherOdd(double num) {
+    double roundedUp = std::ceil(num);
+    return (static_cast<int>(roundedUp) % 2 == 0) ? roundedUp + 1 : roundedUp + 0;
+}
+
+double roundToLowerOdd(double num) {
+    double roundedDown = std::floor(num);
+    return (static_cast<int>(roundedDown) % 2 == 0) ? roundedDown - 1 : roundedDown - 0;
+}
---
+++ functionality/OddRound.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+/// @brief Round up to the nearest odd number
+/// @param num: number to round
+/// @return rounded number
+double roundToHigherOdd(double num);
+
+/// @brief Round down to the nearest odd number
+/// @param num: number to round 
+/// @return rounded number
+double roundToLowerOdd(double num);
