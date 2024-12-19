5961447eb30f942e5e589f5a7de6285f6983f2b2
Tuan Nguyen <nguyendt03@gmail.com>
2024-12-19 14:43:03 +0800

    is odd is even to utils file

---
+++ functionality/Utilities.cpp
@@ -0,0 +1,10 @@
+#include "Utilities.hpp"
+
+
+bool isEven(int num) {
+    return num % 2 == 0;
+}
+
+bool isOdd(int num) {
+    return num % 2 != 0;
+}
---
+++ functionality/Utilities.hpp
@@ -0,0 +1,12 @@
+
+/// @brief The function speaks for itself
+/// @param num integer only
+/// @return true/false
+bool isOdd(int num);
+
+
+
+/// @brief Are you literally dumb to read this
+/// @param num integer only
+/// @return true/false
+bool isEven(int num);
