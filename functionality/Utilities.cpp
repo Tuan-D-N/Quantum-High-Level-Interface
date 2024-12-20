246fa574aeffb66a2b9bdd979a7e5d07ae34b512
Tuan Nguyen <nguyendt03@gmail.com>
2024-12-20 09:58:52 +0800

    added streamable concept and print matrix functions

--- functionality/Utilities.cpp
+++ functionality/Utilities.cpp
@@ -1,4 +1,7 @@
 #include "Utilities.hpp"
+#include <vector>
+#include <iostream>
+#include <complex>
 
 
 bool isEven(int num) {
@@ -8,3 +11,35 @@ bool isEven(int num) {
 bool isOdd(int num) {
     return num % 2 != 0;
 }
+
+
+
+template<Streamable T>
+void print2DVector(const std::vector<std::vector<T>>& vec) {
+    for (const auto& row : vec) {  // Iterate over each row
+        for (T val : row) {       // Iterate over each column in the row
+            std::cout << val << " ";  // Print the element
+        }
+        std::cout << std::endl;  // Move to the next line after printing a row
+    }
+}
+
+template void print2DVector<double>(const std::vector<std::vector<double>>& vec);
+template void print2DVector<int>(const std::vector<std::vector<int>>& vec);
+template void print2DVector<float>(const std::vector<std::vector<float>>& vec);
+
+
+
+template<Streamable T>
+void printVector(const std::vector<T>& vec) {
+    for (T val : vec) {  // Iterate over the vector and print each element
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+}
+
+template void printVector<double>(const std::vector<double>& vec);
+template void printVector<int>(const std::vector<int>& vec);
+template void printVector<float>(const std::vector<float>& vec);
+
+
--- functionality/Utilities.hpp
+++ functionality/Utilities.hpp
@@ -1,3 +1,6 @@
+#include <vector>
+#include <complex>
+
 
 /// @brief The function speaks for itself
 /// @param num integer only
@@ -10,3 +13,23 @@ bool isOdd(int num);
 /// @param num integer only
 /// @return true/false
 bool isEven(int num);
+
+
+// Define a concept for types that can be used with std::ostream
+template <typename T>
+concept Streamable = requires(std::ostream& os, T value) {
+    { os << value } -> std::same_as<std::ostream&>; // Ensures os << value is valid and returns std::ostream&
+};
+
+
+/// @brief Print a 2D vector out 
+/// @tparam T a streamable type
+/// @param vec input vector
+template<Streamable T> void print2DVector(const std::vector<std::vector<T>>& vec);
+
+/// @brief print a 1D vector out
+/// @tparam T a streamable type
+/// @param vec input vector
+template<Streamable T> void printVector(const std::vector<T>& vec);
+
+
