82fea7297211531e6b0a027c231ddd8d93852962
Tuan Nguyen <nguyendt03@gmail.com>
2025-01-06 14:50:23 +0800

    typo

--- functionality/WriteAdjMat.hpp
+++ functionality/WriteAdjMat.hpp
@@ -4,9 +4,9 @@
 using complex = cuDoubleComplex;
 
 
-int getRowOffsetSize(int evenQubits);
-int getColumnIndexSize(int evenQubits);
-int getValuesSize(int evenQubits);
+int getRowOffsetSizeMini(int evenQubits);
+int getColumnIndexSizeMini(int evenQubits);
+int getValuesSizeMini(int evenQubits);
 
 
 void writeMatACSR(int* rowOffset, int* columnIndex, complex* values, int evenQubits, int &rowOffsetSize, int &columnIndexSize, int &valuesSize);
