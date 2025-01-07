a313f2e8a20933471b44cc4ed7b8e3d91ba79606
Tuan Nguyen <nguyendt03@gmail.com>
2025-01-07 14:24:38 +0800

    updated cmake

--- main.cpp
+++ main.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
-#include <cuComplex.h>
-#include "functionality/WriteAdjMat.hpp"
-#include "functionality/GetAdjMat.hpp"
-#include "functionality/Utilities.hpp"
 #include "CudaControl/Sys.hpp"
-#include <vector>
-#include <string>
-#include <fstream>
-#include <sstream>
+
 
 int main()
 {
