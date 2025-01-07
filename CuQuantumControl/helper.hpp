7e445b1581c1672b5e12d3b7be4cfa8ce8174dda
Tuan Nguyen <nguyendt03@gmail.com>
2025-01-07 17:29:48 +0800

    bug fix

--- CuQuantumControl/helper.hpp
+++ CuQuantumControl/helper.hpp
@@ -1,5 +1,5 @@
-#include <cuComplex.h>
 #pragma once
+#include <cuComplex.h>
 
 #define HANDLE_ERROR(x)                                                        \
 {   const auto err = x;                                                        \
@@ -15,14 +15,6 @@
                cudaGetErrorString(err), __LINE__); return err; }               \
 };
 
-bool almost_equal(cuDoubleComplex x, cuDoubleComplex y) {
-    const double eps = 1.0e-5;
-    const cuDoubleComplex diff = cuCsub(x, y);
-    return (cuCabs(diff) < eps);
-}
+bool almost_equal(cuDoubleComplex x, cuDoubleComplex y);
 
-bool almost_equal(double x, double y) {
-    const double eps = 1.0e-5;
-    const double diff = x - y;
-    return (abs(diff) < eps);
-}
+bool almost_equal(double x, double y);
