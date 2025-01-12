e3222ebf2903982b32215d53cd8b94a01182b823
Tuan Nguyen <nguyendt03@gmail.com>
2025-01-12 23:33:46 +0800

    made changes to cmakefile and added not exe

---
+++ groverBenchmarking.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Runner/CircuitTest.hpp"
+#include "CudaControl/Helper.hpp"
+
+int main(int argc, char const *argv[])
+{
+    auto qubit_count = 1 < argc ? atoi(argv[1]) : 25;
+    CHECK_BROAD_ERROR(grover(qubit_count));
+    return 0;
+}
