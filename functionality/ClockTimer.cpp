5b14e0f8dff33e69a5bcdc2f5b49d2495e01a244
Tuan Nguyen <nguyendt03@gmail.com>
2025-01-12 23:58:49 +0800

    fixed timer

--- functionality/ClockTimer.cpp
+++ functionality/ClockTimer.cpp
@@ -4,15 +4,15 @@ Timer::Timer()
 {
     start_m = std::chrono::high_resolution_clock::now();
 }
-Timer::Timer(std::string name) : name_m(name)
-{
-    start_m = std::chrono::high_resolution_clock::now();
+Timer::Timer(std::string name) : Timer() {
+    name_m = std::move(name); // Initialize name_m in the body
 }
+
 
 Timer::~Timer()
 {
     stop_m = std::chrono::high_resolution_clock::now();
     duration_m = std::chrono::duration<double>(stop_m - start_m);
-    std::cout << name_m << "," << duration_m.count() << std::endl;
+    std::cout << name_m << ", Time = " << duration_m.count() << std::endl;
 }
 
