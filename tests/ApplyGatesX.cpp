840d16b002f813790e9e7bba04c10a60995e562b
Tuan Nguyen <nguyendt03@gmail.com>
2025-01-22 10:39:41 +0800

    fix number of qubits

--- tests/ApplyGatesX.cpp
+++ tests/ApplyGatesX.cpp
@@ -220,7 +220,7 @@ TEST_F(ApplyXTestSpan, X_MultipleTargets_SingleControls)
 
 TEST_F(ApplyXTestSpan, X_MultipleTargets_MultipleControls)
 {
-    const int nQubits = 3;
+    const int nQubits = 4;
     std::vector<cuDoubleComplex> input = {{1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0}, {7, 0}, {8, 0}, {9, 0}, {10, 0}, {11, 0}, {12, 0}, {13, 0}, {14, 0}, {15, 0}, {16, 0}};
     std::vector<cuDoubleComplex> expectedOutput = {{1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0}, {7, 0}, {8, 0}, {9, 0}, {10, 0}, {11, 0}, {12, 0}, {16, 0}, {15, 0}, {14, 0}, {13, 0}};
     std::vector<int> targets = {1, 0};
