7473acf3ddca78a079fb75d328fc21a1592f41f6
Tuan Nguyen <nguyendt03@gmail.com>
2025-01-29 09:59:47 +0800

    more testing

--- rawTestering.cu
+++ rawTestering.cu
@@ -5,6 +5,7 @@
 #include <tuple>
 #include "CudaQControl/optimizingSystem.hpp"
 #include "CuQuantumControl/ApplyGates.hpp"
+#include "CuQuantumControl/Utilities.hpp"
 #include "functionality_image/LoadImage.hpp"
 #include "functionality_image/FilterAndLabel.hpp"
 #include "functionality/SquareNorm.hpp"
@@ -98,7 +99,52 @@ public:
     }
 };
 
-optimizingSystemBase loadDataReader(circuitClass &circuitOBJ)
+class optimizingSystemLoader : public optimizingSystemBase
+{
+    using optimizingSystemBase::optimizingSystemBase;
+
+    virtual double lossFunction(const std::vector<double> &paramsVector) override
+    {
+        double totalLoss = 0;
+
+        for (int i = 0; i < m_x_data.size(); ++i)
+        {
+            const auto label = m_y_labels[i];
+
+            const std::vector<float> in_SV = m_x_data[i]; // Make a copy
+
+            std::span<cuComplex> out_SV = m_circuit(in_SV, paramsVector);
+
+            auto binaryResult = measure1QubitUnified<precision::bit_32>(out_SV);
+
+            // if (label == 0)
+            // {
+            //     totalLoss -= std::log(std::abs(binaryResult.first + 0.01));
+            // }
+            // else if (label == 1)
+            // {
+            //     totalLoss -= std::log(std::abs(binaryResult.second + 0.01));
+            // }
+            if (label == 0)
+            {
+                totalLoss -= std::log(magnitude(out_SV.front()) + 0.01);
+            }
+            else if (label == 1)
+            {
+                totalLoss -= std::log(magnitude(out_SV.back()) + 0.01);
+            }
+            else
+            {
+                std::cerr << "label of number: " << label << " was shown.";
+                throw std::logic_error("Expected only 2 label types");
+            }
+        }
+
+        return totalLoss;
+    }
+};
+
+optimizingSystemLoader loadDataReader(circuitClass &circuitOBJ)
 {
     const int INPUTWIDTH = 28, INPUTHEIGHT = 28;
     const int TARGETWIDTH = 32, TARGETHEIGHT = 32;
@@ -128,18 +174,16 @@ optimizingSystemBase loadDataReader(circuitClass &circuitOBJ)
     normalize_and_pad(x_train, INPUTWIDTH, INPUTHEIGHT, TARGETWIDTH, TARGETHEIGHT);
     normalize_and_pad(x_test, INPUTWIDTH, INPUTHEIGHT, TARGETWIDTH, TARGETHEIGHT);
 
+    display_images(x_train, y_train, 32, 32, 10);
     square_normalise_all(x_train);
     square_normalise_all(x_test);
-
 
     save_vector_of_vectors(x_train, "x_train");
     save_vector(y_train, "y_train");
     save_vector_of_vectors(x_test, "x_test");
     save_vector(y_test, "y_test");
 
-    display_images(x_train, y_train, 32, 32, 10);
-
-    optimizingSystemBase optimizerOBJ = optimizingSystemBase(
+    optimizingSystemLoader optimizerOBJ = optimizingSystemLoader(
         std::move(x_train),
         std::move(y_train),
         std::move(circuitOBJ.getCircuitFunction()));
@@ -147,12 +191,12 @@ optimizingSystemBase loadDataReader(circuitClass &circuitOBJ)
     return optimizerOBJ;
 }
 
-optimizingSystemBase loadDataSaved(circuitClass &circuitOBJ)
+optimizingSystemLoader loadDataSaved(circuitClass &circuitOBJ)
 {
     std::vector<std::vector<float>> x_train = load_vector_of_vectors<float>("x_train");
     std::vector<int> y_train = load_vector<int>("y_train");
 
-    optimizingSystemBase optimizerOBJ = optimizingSystemBase(
+    optimizingSystemLoader optimizerOBJ = optimizingSystemLoader(
         std::move(x_train),
         std::move(y_train),
         std::move(circuitOBJ.getCircuitFunction()));
@@ -164,17 +208,20 @@ int main()
 {
     circuitClass circuitOBJ = circuitClass(3, 10);
 
-    optimizingSystemBase optimizerOBJ = loadDataReader(circuitOBJ);
+    optimizingSystemLoader optimizerOBJ = loadDataReader(circuitOBJ);
     auto optimizer = cudaq::optimizers::adam();
-    optimizer.max_eval = 2;
+    optimizer.max_eval = 10000;
     optimizer.initial_parameters = std::vector<double>{-1.04636, -11.247, -28.1384, -25.2752, 9.91089, 43.9357, -15.5106, 5.88025, 10.6416, -9.45491, 24.5154, -33.4887, 6.31566, 18.5741, 9.52134, 8.17428, -3.59727, -36.4042, 0.688819, -23.659, -26.5258, 29.6948, -1.63063, 35.7586, 17.3797, 5.47379, -11.5945, 19.4702, 4.38039, 3.18327};
 
     std::vector<double> gradient;
-    std::vector<double> init{-1.04636, -11.247, -28.1384, -25.2752, 9.91089, 43.9357, -15.5106, 5.88025, 10.6416, -9.45491, 24.5154, -33.4887, 6.31566, 18.5741, 9.52134, 8.17428, -3.59727, -36.4042, 0.688819, -23.659, -26.5258, 29.6948, -1.63063, 35.7586, 17.3797, 5.47379, -11.5945, 19.4702, 4.38039, 3.18327};
+    std::vector<double> init{0.129166, -10.991, -31.5984, -25.1872, 11.136, 43.9733, -20.3166, 6.3193, 8.41446, -10.0209, 25.0944, -35.2623, 9.56064, 17.4881, 7.96029, 10.912, 1.47141, -36.1338, 1.46848, -29.9286, -26.6139, 34.286, -1.4129, 34.4916, 15.6386, 4.65735, -12.5786, 14.9738, 2.38349, -1.50437};
     auto objectiveFunction = (optimizerOBJ.getObjectiveFunction());
-    auto result = optimizer.optimize(circuitOBJ.getNumberOfParams(), cudaq::optimizable_function(objectiveFunction));
     std::cout << "\ndone\n";
     double value = optimizerOBJ.getObjectiveFunction()(init, gradient);
+    std::cout << "final value: " << value << "\n";
+    auto result = optimizer.optimize(circuitOBJ.getNumberOfParams(), cudaq::optimizable_function(objectiveFunction));
+    std::cout << "\ndone\n";
+    value = optimizerOBJ.getObjectiveFunction()(init, gradient);
     std::cout << "final value: " << value << "\n";
 
     auto energy = std::get<0>(result);
