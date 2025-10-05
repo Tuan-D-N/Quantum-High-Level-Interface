8f0751ac7f8fba337a8fc6f63f12340f9024df1b
Tuan Dao Nguyen <nguyendt03@gmail.com>
2025-10-05 23:48:59 +0800

    added static to global device functions

--- CuSparseControl/SparseHelper.hpp
+++ CuSparseControl/SparseHelper.hpp
@@ -6,14 +6,14 @@
 // ===============================
 // Gather / Scatter kernels
 // ===============================
-__global__ void gather_kernel(cuDoubleComplex *d_out, const cuDoubleComplex *d_in, const int *d_offsets, int size)
+static __global__ void gather_kernel(cuDoubleComplex *d_out, const cuDoubleComplex *d_in, const int *d_offsets, int size)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < size)
         d_out[i] = d_in[d_offsets[i]];
 }
 
-__global__ void scatter_kernel(cuDoubleComplex *d_out, const cuDoubleComplex *d_in, const int *d_offsets, int size)
+static __global__ void scatter_kernel(cuDoubleComplex *d_out, const cuDoubleComplex *d_in, const int *d_offsets, int size)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < size)
