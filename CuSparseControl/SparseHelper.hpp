921cdf89417397b204d5bd7709fd0b2061892ca0
Tuan Nguyen <nguyendt03@gmail.com>
2025-10-13 18:24:22 +0800

    Fixed SparseHelper to support both int and ulonglong int

--- CuSparseControl/SparseHelper.hpp
+++ CuSparseControl/SparseHelper.hpp
@@ -19,3 +19,17 @@ static __global__ void scatter_kernel(cuDoubleComplex *d_out, const cuDoubleComp
     if (i < size)
         d_out[d_offsets[i]] = d_in[i];
 }
+
+static __global__ void gather_kernel(cuDoubleComplex *d_out, const cuDoubleComplex *d_in, const int*d_offsets, int size)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < size)
+        d_out[i] = d_in[d_offsets[i]];
+}
+
+static __global__ void scatter_kernel(cuDoubleComplex *d_out, const cuDoubleComplex *d_in, const int*d_offsets, int size)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < size)
+        d_out[d_offsets[i]] = d_in[i];
+}
